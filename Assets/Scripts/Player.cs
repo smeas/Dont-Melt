39b3970384050094e856c82e8a239b4df79c6425
Jonatan Johansson <smeas@users.noreply.github.com>
2020-10-26 18:21:04 +0100

    Add Player script with health that affects the scale

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+	[SerializeField] private int health = 100;
+	[SerializeField] private int maxHealth = 100;
+	[SerializeField] private float minScale = 0.2f;
+	[SerializeField] private float maxScale = 1f;
+
+	public int Health
+	{
+		get => health;
+		set
+		{
+			health = Mathf.Clamp(value, 0, maxHealth);
+			UpdatePlayerSize();
+		}
+	}
+
+	public int MaxHealth => maxHealth;
+
+	private void Start()
+	{
+		UpdatePlayerSize();
+	}
+
+	private void OnValidate()
+	{
+		UpdatePlayerSize();
+	}
+
+	private void UpdatePlayerSize()
+	{
+		float scale = Mathf.Lerp(minScale, maxScale, (float)health / maxHealth);
+		transform.localScale = new Vector3(scale, scale, scale);
+	}
+}
