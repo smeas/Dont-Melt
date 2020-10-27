d62da4429f7bcd0e558895207501b526f6b4d046
Jonatan Johansson <smeas@users.noreply.github.com>
2020-10-27 10:12:20 +0100

    Add health pickup

---
+++ Assets/Scripts/Level Elements/HealthPickup.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Collider2D))]
+public class HealthPickup : MonoBehaviour
+{
+	[SerializeField] private int healthValue = 5;
+
+	private void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.CompareTag("Player"))
+		{
+			Player player = other.GetComponent<Player>();
+			if (player != null)
+			{
+				player.Health += healthValue;
+				Destroy(gameObject);
+			}
+		}
+	}
+}
