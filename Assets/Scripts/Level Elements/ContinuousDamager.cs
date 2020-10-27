5fbb95a8260a143b9ba481f0fd2bdd4647f0c28a
Jonatan Johansson <smeas@users.noreply.github.com>
2020-10-27 12:13:55 +0100

    Add ContinuousDamager and DamageArea

---
+++ Assets/Scripts/Level Elements/ContinuousDamager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class ContinuousDamager : MonoBehaviour
+{
+	[SerializeField, Tooltip("The player to damage. (optional)")]
+	private Player player = null;
+
+	[SerializeField] private float damagePerSecond = 1;
+
+	private void OnEnable()
+	{
+		if (player == null)
+		{
+			GameObject playerObject = GameObject.FindGameObjectWithTag("Player");
+			if (playerObject != null)
+				player = playerObject.GetComponent<Player>();
+		}
+	}
+
+	private void Update()
+	{
+		if (player != null)
+			player.Health -= damagePerSecond * Time.deltaTime;
+	}
+}
---
+++ Assets/Scripts/Level Elements/DamageArea.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider2D))]
+public class DamageArea : MonoBehaviour
+{
+	private enum DamageMode
+	{
+		Single,
+		Continuous,
+	}
+
+	[SerializeField] private DamageMode mode = DamageMode.Single;
+	[SerializeField] private float damage = 10;
+
+	private void OnTriggerEnter2D(Collider2D other) => OnEnter(other);
+	private void OnCollisionEnter2D(Collision2D collision) => OnEnter(collision.collider);
+	private void OnTriggerStay2D(Collider2D other) => OnStay(other);
+	private void OnCollisionStay2D(Collision2D collision) => OnStay(collision.collider);
+
+	private void OnEnter(Collider2D other)
+	{
+		if (mode != DamageMode.Single) return;
+
+		Player player = GetPlayer(other);
+		if (player != null)
+			player.Health -= damage;
+	}
+
+	private void OnStay(Collider2D other)
+	{
+		if (mode != DamageMode.Continuous) return;
+
+		Player player = GetPlayer(other);
+		if (player != null)
+			player.Health -= damage * Time.deltaTime;
+	}
+
+	private static Player GetPlayer(Collider2D other)
+	{
+		return other.CompareTag("Player") ? other.GetComponent<Player>() : null;
+	}
+}
