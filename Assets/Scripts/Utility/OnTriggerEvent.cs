097e4d44306919d2bc44335c4b3f6e43868887dd
Jonatan Johansson <smeas@users.noreply.github.com>
2020-10-27 15:27:16 +0100

    Update Ice level

---
+++ Assets/Scripts/Utility/OnTriggerEvent.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class OnTriggerEvent : MonoBehaviour
+{
+	[SerializeField] private bool onlyTriggerOnce = false;
+	[SerializeField] private UnityEvent onTriggerEnter = null;
+
+	private bool hasTriggered;
+
+	private void OnTriggerEnter2D(Collider2D other)
+	{
+		if (onlyTriggerOnce && hasTriggered) return;
+
+		onTriggerEnter.Invoke();
+		hasTriggered = true;
+	}
+}
