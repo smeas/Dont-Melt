51200f9e1f4d23af02c3dbaed1ef817a60987714
Jonatan Johansson <smeas@users.noreply.github.com>
2020-10-28 19:24:28 +0100

    Add ImpactDamager.cs and DisableOnLoad.cs

---
+++ Assets/Scripts/Level Elements/ImpactDamager.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using UnityEngine;
+
+public class ImpactDamager : MonoBehaviour
+{
+	[SerializeField] private float damage = 10;
+
+	[SerializeField, Tooltip("The minimum amount of impact velocity required to damage the player.")]
+	private float minImpactVelocity = 6;
+
+	private new Rigidbody2D rigidbody;
+	private Vector2 previousVelocity;
+
+	private void Awake()
+	{
+		rigidbody = GetComponent<Rigidbody2D>();
+	}
+
+	private void FixedUpdate()
+	{
+		previousVelocity = rigidbody.velocity;
+	}
+
+	private void OnCollisionEnter2D(Collision2D collision)
+	{
+		if (previousVelocity.sqrMagnitude >= minImpactVelocity * minImpactVelocity &&
+			collision.collider.CompareTag("Player"))
+		{
+			Player player = collision.collider.GetComponent<Player>();
+			player.TakeDamage(damage);
+		}
+	}
+}
---
+++ Assets/Scripts/Utility/DisableOnLoad.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using UnityEngine;
+
+public class DisableOnLoad : MonoBehaviour
+{
+	private void Awake()
+	{
+		gameObject.SetActive(false);
+	}
+}
