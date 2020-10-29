72e436328ec816434d79acdf5921c5d451835560
Jonatan Johansson <smeas@users.noreply.github.com>
2020-10-29 15:10:54 +0100

    Update ice level

---
+++ Assets/Scripts/Level Elements/PolarBear.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using UnityEngine;
+
+public class PolarBear : MonoBehaviour
+{
+	[SerializeField] private Vector2 speed;
+
+	private void Update()
+	{
+		transform.position += (Vector3)(speed * Time.deltaTime);
+	}
+}
