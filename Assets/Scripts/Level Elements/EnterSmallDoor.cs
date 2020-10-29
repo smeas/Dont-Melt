35d06896ca8b5b9c621dde4c5a45a5097ad27a12
danielalexandernielsen <56228796+danielalexandernielsen@users.noreply.github.com>
2020-10-29 17:28:29 +0100

    Spikes now does damage, small door teleports, added goal pickup

---
+++ Assets/Scripts/Level Elements/EnterSmallDoor.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Cinemachine;
+using UnityEngine;
+
+public class EnterSmallDoor : MonoBehaviour
+{
+    private Player player;
+    public GameObject connectedDoor;
+
+    private bool keyDown;
+    private bool atDoor;
+    private CinemachineBrain cameraBrain;
+
+    private void OnEnable()
+    {
+        if (player == null)
+        {
+            GameObject playerObject = GameObject.FindGameObjectWithTag("Player");
+            if (playerObject != null)
+                player = playerObject.GetComponent<Player>();
+        }
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (atDoor && Input.GetKeyDown(KeyCode.W))
+        {
+            keyDown = true;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        atDoor = true;
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        atDoor = false;
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (keyDown)
+        {
+            Vector3 offset = connectedDoor.transform.position - player.transform.position;
+            player.transform.position += offset;
+
+            cameraBrain = Camera.main.GetComponent<CinemachineBrain>();
+            CinemachineVirtualCamera virtualCamera = cameraBrain.ActiveVirtualCamera.VirtualCameraGameObject.GetComponent<CinemachineVirtualCamera>();
+            virtualCamera.OnTargetObjectWarped(player.transform, offset);
+
+            keyDown = false;
+        }
+    }
+}
