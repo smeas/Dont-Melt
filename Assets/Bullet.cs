74fbd64880c9a08cc650ccf3bd74d7d974875248
danielalexandernielsen <56228796+danielalexandernielsen@users.noreply.github.com>
2020-10-29 18:44:42 +0100

    Cannons shoot fireballs
    Co-Authored-By: Jonatan Johansson <smeas@users.noreply.github.com>

---
+++ Assets/Bullet.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+
+    Vector2 velocity;
+    [SerializeField] private float destroyAfterXSeconds = 10;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(gameObject, destroyAfterXSeconds);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += transform.rotation * ((Vector3)velocity * Time.deltaTime);
+    }
+
+    public void Fire(Vector2 velocity)
+    {
+        this.velocity = velocity;
+    }
+
+}
---
+++ Assets/Cannon.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cannon : MonoBehaviour
+{
+
+    [SerializeField] private Vector2 velocity = new Vector2(-4, 0);
+    [SerializeField] private float bulletsPerSecond = 2;
+    [SerializeField] private Bullet bullet;
+    private float accumlatedTime;
+    private bool closeToCannon;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        accumlatedTime += Time.deltaTime;
+
+        if ((accumlatedTime > bulletsPerSecond) && closeToCannon)
+        {
+            Fire();
+            accumlatedTime = 0;
+        }
+    }
+
+    public void Fire()
+    {
+        Bullet newBullet;
+
+        newBullet = Instantiate(bullet, transform.position, transform.rotation);
+        newBullet.transform.localScale = transform.localScale;
+        newBullet.Fire(velocity);
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        closeToCannon = true;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        closeToCannon = false;
+    }
+
+}
