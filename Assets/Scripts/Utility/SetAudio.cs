65dd8cf36977526eb3fd9416243bb84bfb119cfc
RobinVondrak <robinvondrak01@gmail.com>
2020-10-29 11:52:37 +0100

    Sound settings

---
+++ Assets/Scripts/Utility/SetAudio.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class SetAudio : MonoBehaviour
+{
+    [SerializeField] private AudioMixer audioMixer;
+    [Header("Sliders")]
+    [SerializeField] private Slider master;
+    [SerializeField] private Slider music;
+    [SerializeField] private Slider effects;
+
+    private void Start()
+    {
+        if (audioMixer == null)
+            Debug.LogError("Missing Audio Mixer");
+    }
+    public void SetMasterVolume()
+    {
+        audioMixer.SetFloat("masterVolume", master.value);
+    }
+    public void SetMusicVolume()
+    {
+        audioMixer.SetFloat("musicVolume", music.value);
+    }
+    public void SetEffectsVolume()
+    {
+        audioMixer.SetFloat("sfxVolume", effects.value);
+    }
+}
