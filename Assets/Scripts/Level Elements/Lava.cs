867456072d96dcfa119bdb62335807eaaed471b6
danielalexandernielsen <56228796+danielalexandernielsen@users.noreply.github.com>
2020-10-29 15:08:21 +0100

    Added teleportation between doors, water & lava effectors

---
+++ Assets/Scripts/Level Elements/Lava.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lava : MonoBehaviour
+{
+
+    [SerializeField] private float damagePerSecond = 1;
+    private Player player;
+    bool inLava;
+
+    private void OnEnable()
+    {
+        if (player == null)
+        {
+            GameObject playerObject = GameObject.FindGameObjectWithTag("Player");
+            if (playerObject != null)
+                player = playerObject.GetComponent<Player>();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (inLava)
+        {
+            if (player != null)
+                player.TakeDamage(damagePerSecond * Time.deltaTime);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        inLava = true;
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        inLava = false;
+    }
+}
---
+++ Assets/Scripts/Level Elements/enterDoor.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enterDoor : MonoBehaviour
+{
+    private Player player;
+    public GameObject connectedDoor;
+    bool enterThisDoor;
+
+    private void OnEnable()
+    {
+        if (player == null)
+        {
+            GameObject playerObject = GameObject.FindGameObjectWithTag("Player");
+            if (playerObject != null)
+                player = playerObject.GetComponent<Player>();
+        }
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (enterThisDoor)
+        {
+            player.transform.position = connectedDoor.transform.position;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            enterThisDoor = true;
+        }
+    }
+
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            enterThisDoor = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        enterThisDoor = false;
+    }
+}
