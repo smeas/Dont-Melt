293549c8df7556467965839787aa14357121b0e3
danielalexandernielsen <56228796+danielalexandernielsen@users.noreply.github.com>
2020-10-30 09:38:48 +0100

    Added bird sprites, walking critter script not finished yet

---
+++ Assets/WalkingCritter.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WalkingCritter : MonoBehaviour
+{
+
+    [SerializeField] private GameObject LeftObject;
+    [SerializeField] private GameObject RightObject;
+    [SerializeField] private Vector2 walkingSpeed = new Vector2(1, 0);
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        transform.position += (Vector3) walkingSpeed;
+
+    }
+}
