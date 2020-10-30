b6475b87c6de4e8241854536806394ac427e5185
Jonatan Johansson <smeas@users.noreply.github.com>
2020-10-30 21:53:31 +0100

    Small fixes and tweaks part 2

--- Assets/Scripts/Level Elements/ChasingSun.cs
+++ Assets/Scripts/Level Elements/ChasingSun.cs
@@ -8,7 +8,9 @@ public class ChasingSun : MonoBehaviour
 	[SerializeField] private float maxSpeed = 10;
 	[SerializeField] private float smoothTime = 1;
 	[SerializeField] private float damagePerSecond = 10;
-	[SerializeField] private Transform rays;
+	[SerializeField] private Transform rays = null;
+	[SerializeField] private Color coldTint = Color.white;
+	[SerializeField] private Color hotTint = Color.white;
 
 	private Player player;
 	private Transform playerTransform;
@@ -18,11 +20,17 @@ public class ChasingSun : MonoBehaviour
 	private Vector3 raycastHitPoint;
 	private bool raycastHitPlayer;
 
+	private new SpriteRenderer renderer;
+	private SpriteRenderer raysRenderer;
+
 	private void Start()
 	{
 		GameObject playerObject = GameObject.FindGameObjectWithTag("Player");
 		player = playerObject.GetComponent<Player>();
 		playerTransform = playerObject.transform;
+
+		renderer = GetComponent<SpriteRenderer>();
+		raysRenderer = rays.GetComponent<SpriteRenderer>();
 
 		offsetFromPlayer = transform.position - playerTransform.position;
 	}
@@ -33,11 +41,12 @@ public class ChasingSun : MonoBehaviour
 		transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref currentVelocity, smoothTime, maxSpeed);
 
 		rayDirection = playerTransform.position - transform.position;
-
 		float rayAngle = Mathf.Atan2(rayDirection.y, rayDirection.x) * Mathf.Rad2Deg;
 		rays.eulerAngles = new Vector3(0, 0, rayAngle);
 
 		UpdateRaycast();
+
+		renderer.color = raysRenderer.color = raycastHitPlayer ? hotTint : coldTint;
 
 		if (raycastHitPlayer)
 		{
