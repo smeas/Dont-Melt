7eac5069cb1a99c6e5843569ebeea1252c7315e3
danielalexandernielsen <56228796+danielalexandernielsen@users.noreply.github.com>
2020-10-30 10:00:59 +0100

    Critter walking script finished

--- Assets/WalkingCritter.cs
+++ Assets/WalkingCritter.cs
@@ -8,13 +8,33 @@ public class WalkingCritter : MonoBehaviour
     [SerializeField] private GameObject LeftObject;
     [SerializeField] private GameObject RightObject;
     [SerializeField] private Vector2 walkingSpeed = new Vector2(1, 0);
-    
+    [SerializeField] private bool flipSpriteToggle = true;
+    private SpriteRenderer sprite;
+
+
+    private void Start()
+    {
+        sprite = gameObject.GetComponent<SpriteRenderer>();
+        sprite.flipX = flipSpriteToggle;
+    }
 
     // Update is called once per frame
     void Update()
     {
 
-        transform.position += (Vector3) walkingSpeed;
+        if ((transform.position.x < LeftObject.transform.position.x) || (transform.position.x > RightObject.transform.position.x))
+        {
+            walkingSpeed *= -1;
+            FlipSprite();
+        }
 
+        transform.position += (Vector3) walkingSpeed * Time.deltaTime;
     }
+
+    void FlipSprite()
+    {
+        flipSpriteToggle = !flipSpriteToggle;
+        sprite.flipX = flipSpriteToggle;
+    }
+
 }
